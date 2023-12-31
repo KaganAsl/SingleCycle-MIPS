cb56ceaf758a2b40ac4512184450dac3b01288df
KaganAsl <kagan.aslancan@gmail.com>
2023-12-31 17:53:52 +0300

    Preparing alu for jump instructions

--- alu_32.v
+++ alu_32.v
@@ -1,17 +1,18 @@
 module alu_32(op1, op2, alu_cont, alu_result, z);
 input [31:0] op1, op2;
-input [2:0] alu_cont;
+input [3:0] alu_cont;
 output reg [31:0] alu_result;
 output reg z;
 reg [31:0] less;
 always @(op1 or op2 or alu_cont)
 begin
 	case(alu_cont)
-	3'b010 : alu_result = op1 + op2;
-	3'b110 : alu_result = op1 + 1 + ~op2;
-	3'b000 : alu_result = op1 & op2;
-	3'b001 : alu_result = op1 | op2;
-	3'b111 : 
+	4'b0010 : alu_result = op1 + op2;
+	4'b0110 : alu_result = op1 + 1 + ~op2;
+	4'b0000 : alu_result = op1 & op2;
+	4'b0001 : alu_result = op1 | op2;
+	4'b1000 : alu_result = op2;
+	4'b0111 : 
 		begin
 			less = op1 + 1 + ~op2;
 			if(less[31])
@@ -20,7 +21,7 @@ begin
 			alu_result = 0;
 		end
 	endcase
-z = ~(| alu_result);
+z = ~(|alu_result);
 end
 
 endmodule
--- alu_cont.v
+++ alu_cont.v
@@ -1,24 +1,29 @@
-module alu_cont(alu_op1, alu_op0, f3, f2, f1, f0, out);
-input alu_op1, alu_op0, f3, f2, f1, f0;
+module alu_cont(alu_op1, alu_op0, f_bits, out);
+input alu_op1, alu_op0;
+input [5:0] f_bits;
 output reg [2:0] out;
-always @(alu_op1 or alu_op0 or f3 or f2 or f1 or f0)
+always @(alu_op1 or alu_op0 or f_bits)
 begin
 	if(~(alu_op1 | alu_op0))
-	out = 3'b010;
+	out = 4'b0010;
 	if(alu_op0)
-	out = 3'b110;
+	out = 4'b0110;
 	if(alu_op1)
 	begin
-		if(~(f3 | f2 | f1 | f0))
-		out = 3'b010;
-		if(~f3 & ~f2 & f1 & ~f0)
-		out = 3'b110;
-		if(f3 & ~f2 & f1 & f0)
-		out = 3'b000;
-		if(~f3 & f2 & ~f1 & f0)
-		out = 3'b001;
-		if(f3 & ~f2 & f1 & ~f0)
-		out = 3'b111;
+		if(f_bits[5] & ~(|f_bits[4:1]) & f_bits[0])
+		out = 4'b0011;
+		if(f_bits[5] & ~(|f_bits[4:0]))
+		out = 4'b0010;
+		if(f_bits[5] & ~(|f_bits[4:2]) & f_bits[1] & ~f_bits[0])
+		out = 4'b0110;
+		if(f_bits[5] & ~(|f_bits[4:3]) & f_bits[2] & ~(|f_bits[1:0]))
+		out = 4'b0000;
+		if(f_bits[5] & ~(|f_bits[4:3]) & f_bits[2] & ~f_bits[1] & f_bits[0])
+		out = 4'b0001;
+		if(f_bits[5] & ~f_bits[4] & f_bits[3] & ~f_bits[2] & f_bits[1] & ~f_bits[0])
+		out = 4'b0111;
+		if(~(|f_bits[5:4]) & f_bits[3] & ~(|f_bits[2:0]))
+		out = 4'b1000;
 	end
 end
 endmodule
